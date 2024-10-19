5a244c8e544be04229705f32928b412b0b5f7af8
AaronMir <aaron.mir@donvatio.es>
2024-10-19 12:19:11 +0200

    modificaciones codificacion de los ficheros para su apertura falla, csv en la parte anterior

--- web/main.py
+++ web/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request, UploadFile, File 
 import pandas as pd 
 
-
 app = FastAPI()
 
 @app.get("/")
@@ -12,19 +11,17 @@ async def root():
 async def root(request: Request, Fichero_Customers : UploadFile = None, Fichero_solicitudes_de_alta : UploadFile = None):
     
     if Fichero_Customers.content_type == "text/csv" and Fichero_solicitudes_de_alta.content_type == "text/csv":
-        
-        elemento_1_csv = await Fichero_Customers.read(size=32)  
+        elemento_1_csv = await Fichero_Customers.read()  
         customers_csv_format = elemento_1_csv.decode("UTF-16 LE")
         
+        # verificar como solucionar el fallo del decode 
         # elemento_2_csv = await Fichero_solicitudes_de_alta.read(size=32)
         # motivos_csv_format = elemento_2_csv.decode("UTF-16 LE")
         
-        
-        customers = pd.read_csv(customers_csv_format, sep=';')
+        customers = pd.read_csv(customers_csv_format.replace("\ufeff", ''), sep=';', delimiter='"')
         # motivos = pd.read_csv(motivos_csv_format, sep=';')
     
-    
-        print(customers)
+        # print(customers)
         # print(motivos)
     
     # print(Fichero_solicitudes_de_alta)
